f299554314baa430ee0f45572073c99ad66433eb
Aditya Raj <adityarj1910@gmail.com>
2025-12-04 17:16:52 +0530

    fixed URL

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser()); // Parse cookies
 
 // DB
-mongoose.connect(process.env.MONGO_URIB, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
