2b9ef1cd307bc99d1fa291bf8956d426e26fb103
Aditya Raj <adityarj1910@gmail.com>
2025-12-05 02:05:33 +0530

    mongo congif

--- server/server.js
+++ server/server.js
@@ -24,10 +24,7 @@ app.use(express.json());
 app.use(cookieParser()); // Parse cookies
 
 // DB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log("DB Error:", err));
 
