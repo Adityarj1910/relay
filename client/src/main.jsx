ef81447fe4ef6d366672e34a4ff1c79388e568cc
Aditya Raj <adityarj1910@gmail.com>
2025-12-07 20:01:20 +0530

    added vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
-import { Analytics } from "@vercel/analytics/next"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App /> <Analytics />
+      <App />
     </AuthProvider>
   </StrictMode>,
 )
